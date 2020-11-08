30968920aa871d02590db147296316370281f5c7
tidymoon <73995927+tidymoon@users.noreply.github.com>
2020-11-08 16:26:16 +0800

    add newpage

---
+++ js/tj-github.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a68eefcddec688290fe99573669271bf";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
